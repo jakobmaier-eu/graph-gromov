6ad2c430349aa100526f42189811000627333b19
emile-dailly <emdailly@hotmail.com>
2024-07-19 16:52:51 +0200

    Grampa from MATLAB

--- grampa.py
+++ grampa.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 from functions import *
-
+from scipy.linalg import eig
+from scipy.optimize import linear_sum_assignment
 #According to Fan-Mao-Wu-Xu 2019
 
 def grampa(A, B, eta):
@@ -20,3 +21,41 @@ def grampa(A, B, eta):
         for j in range(n):
             K= K + 1/(eta**2 + eigvalA[i]**2 + eigvalB[j]** 2) * (eigvectA[i] * eigvalA[i].T ) @ J @ (eigvectB[j] * eigvalB[j].T)
     return project_to_perm(K, A_eq, b_eq)
+
+def grampa_matlab(A, B, eta):
+    """
+    GRAMPA: Graph Matching by Pairwise Eigen-Alignments
+    
+    Parameters:
+    - A: the (centered or uncentered) adjacency matrix of the first graph
+    - B: the (centered or uncentered) adjacency matrix of the second graph
+    - eta: the regularization parameter (eta > 0)
+    
+    Returns:
+    - P: the permutation matrix P such that P @ A @ P.T is matched to B
+    """
+    n = A.shape[0]
+    
+    # Compute the eigenvalues and eigenvectors
+    lambda_vals, U = eig(A)
+    mu_vals, V = eig(B)
+    
+    lambda_vals = np.real(lambda_vals)
+    mu_vals = np.real(mu_vals)
+    U = np.real(U)
+    V = np.real(V)
+    
+    # Compute the similarity matrix
+    lambda_diff = (lambda_vals[:, None] - mu_vals[None, :]) ** 2
+    coeff = 1.0 / (lambda_diff + eta ** 2)
+    coeff = coeff * (U.T @ np.ones((n, n)) @ V)
+    X = U @ coeff @ V.T
+    
+    # Solve the linear sum assignment problem (Hungarian algorithm)
+    row_ind, col_ind = linear_sum_assignment(-X)
+    
+    # Create the permutation matrix
+    P = np.zeros((n, n))
+    P[row_ind, col_ind] = 1
+    
+    return P
